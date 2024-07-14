b84a3bfa96c5aa07d688062d76c8fe33d7a10478
kdw <50583568+dong336@users.noreply.github.com>
2024-07-14 20:59:34 +0900

    [관리자] 사용자 등록

---
+++ ShopWebParent/ShopBackEnd/src/main/java/com/shop/admin/user/UserController.java
@@ -0,0 +1,52 @@
+package com.shop.admin.user;
+
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import com.shop.common.entity.Role;
+import com.shop.common.entity.User;
+
+import jakarta.annotation.Resource;
+
+@Controller
+public class UserController {
+
+	@Resource
+	private UserService service;
+
+	@GetMapping("/users")
+	public String listAll(Model model) {
+		List<User> listUsers = service.listAll();
+		model.addAttribute("listUsers", listUsers);
+		
+		return "users";
+	}
+	
+	@GetMapping("/users/new")
+	public String newUser(Model model) {
+		List<Role> listRoles = service.listRoles();
+		
+		User user = new User();
+		user.setEnabled(true);
+		
+		model.addAttribute("user", user);
+		model.addAttribute("listRoles", listRoles);
+		
+		return "user_form";
+	}
+	
+	@PostMapping("/users/save")
+	public String saveUser(User user, RedirectAttributes redirectAttributes) {
+		System.out.println(user);
+		service.save(user);
+		
+		redirectAttributes.addFlashAttribute("message", "사용자 등록 성공");
+		
+		return "redirect:/users";
+	}
+}
